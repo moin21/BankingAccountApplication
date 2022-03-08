cad1ffa06a93f989e1333609fed3602733e2510b
moin21 <mamoinuddin@gmail.com>
2022-03-08 08:22:22 +0530

    Added: Savings Account with annual interest rate of 4

---
+++ src/com/bankingaccountapplication/SavingsAccount.java
@@ -0,0 +1,9 @@
+package com.bankingaccountapplication;
+
+public class SavingsAccount {
+	
+	/**
+	 * Static variables - annual interest rate offered by bank.
+	 */
+	public static double annualInterestRate = 4;
+}
